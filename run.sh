3c82a6fa6487d78deb16d9f55e3534bc6643503d
Raghu K <raghuk.vit@gmail.com>
2023-01-27 08:23:35 +0530

    Update app-code/init-container/

--- run.sh
+++ run.sh
@@ -3,11 +3,11 @@
 git clone https://github.com/raghudevopsb69/$component
 cd $component/schema
 
-if [ "dbtype" == "mysql" ]; then
+if [ "$dbtype" == "mysql" ]; then
   mysql -h $dbhost -u$dbuser -p$dbpass <$component.sql
 fi
 
-if [ "dbtype" == "mongo" ]; then
+if [ "$dbtype" == "mongo" ]; then
   curl -L -O https://s3.amazonaws.com/rds-downloads/rds-combined-ca-bundle.pem
   mongo --ssl --host $dbhost:27017 --sslCAFile rds-combined-ca-bundle.pem --username $dbuser --password $dbpass < $component.js
 fi
